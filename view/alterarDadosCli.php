265d2035ab5613a5712d59f1f4c1cf5aa060e31b
Emili Alves de Souza <emilialvessouza@gmail.com>
2022-07-18 11:53:52 -0300

    Add files via upload

---
+++ view/alterarDadosCli.php
@@ -0,0 +1,349 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../View/css/styledados.css">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <title>alterar dados do cliente</title>
+    <?php
+    include_once 'navs/navSairAlt.php';
+    ?>
+</head>
+
+<body>
+
+    <main>
+        <?php
+        session_start();
+        include_once '../controller/verificaLogin.php';
+        require_once '../model/ClienteDao.php';
+        require_once '../model/ProdutoDao.php';
+
+        $clienteDao = new ClienteDao();
+        $produtoDao = new ProdutoDao();
+        $linhas = $clienteDao->read($_SESSION['id']);
+        $linhasProd = $produtoDao->read_prod_id($_SESSION['id']);
+
+
+        foreach ($linhas as $linha) {
+
+        ?>
+            <!-- ALTERAR DADOS CLIENTE -->
+            <h1 class="alterar">Alterar dados do Cliente</h1>
+
+            <div id="form-alterar">
+                <form action="../controller/alterarDadosCli.php" method="post" class="row g-3" id="dados">
+                    <div id="form-alterar-nome" class="col-md-6">
+                        <label for="cxNome" class="form-label">Nome:</label>
+                        <input type="text" class="form-control" name="nome" id="cxNome" value="<?= $linha['nome_cli'] ?>">
+                    </div>
+                    <div id="form-alterar-sobrenome" class="col-md-6">
+                        <label class="form-label" for="cxSobrenome">Sobrenome:</label>
+                        <input class="form-control" type="text" name="sobrenome" id="cxSobrenome" value="<?= $linha['sobrenome_cli'] ?>">
+                    </div>
+                    <div class="col-3" id="form-alterar-data_nasc">
+                        <label class="form-label" for="cxData_nasc">Data de Nascimento:</label>
+                        <input class="form-control" type="text" name="data_nasc" id="cxData_nasc" value="<?= $linha['nascimento_cli'] ?>">
+                    </div>
+                    <div class="col-3" id="form-alterar-cnpj">
+                        <label class="form-label" for="cxCNPJ">CNPJ:</label>
+                        <input class="form-control" maxlength="18" type="text" name="cnpj" id="cxCNPJ" value="<?= $linha['cnpj_cli'] ?>">
+                    </div>
+                    <div class="col-3" id="form-alterar-ramo">
+                        <label class="form-label" for="cxRamo">Ramo:</label>
+                        <input class="form-control" maxlength="18" type="text" name="ramo" id="cxRamo" value="<?= $linha['ramo_cli'] ?>">
+                    </div>
+                    <div class="col-md-6" id="form-alterar-email">
+                        <label class="form-label" for="cxEmail">Email</label>
+                        <input class="form-control" type="email" name="email" id="cxEmail" value="<?= $linha['email_cli'] ?>">
+                    </div>
+                    <div class="col-md-4" id="form-alterar-senha">
+                        <label class="form-label" for="cxSenha">Senha:</label>
+                        <input class="form-control" type="password" name="senha" id="cxSenha">
+
+                    </div>
+                    <div class="col-md-4" id="form-alterar-plano">
+                        <label class="form-label" for="cxPlano">Plano:</label>
+                        <input class="form-control" type="text" name="plano" id="cxPlano" value="<?= $linha['plano_atual'] ?>">
+                    </div>
+
+                    <div class="col-12">
+                        <button id="btnAlterar" type="submit" class="btn"> Alterar dados</button>
+                    </div>
+
+                </form>
+                <!-- ALTERAR DADOS CLIENTE -->
+
+                <!-- ALTERAR DADOS PRODUTO -->
+                <?php
+                if (!$linhasProd == null) {
+                ?>
+
+                    <h1 class="alterar">Alterar dados do Produto</h1>
+                <?php
+                } else {
+                ?>
+                    <h1 class="alterar">Nenhum Produto Cadastrado</h1>
+                <?php
+                }
+                ?>
+                <!-- CADASTRAR PRODUTO -->
+
+                <div class="col-12">
+                    <button id="btn-cad" type="submit" class="btn">Cadastrar</button>
+                </div>
+                <div id="cont" style="display:none; margin-left:300px; padding:15px;" class="card col-md-6" style=" padding:30px; margin-top: 30px; margin-left: 350px;">
+                    <h1 style="text-align: center;">Cadastrar Produto</h1>
+                    <div id="formProd" class="row g-3">
+                        <div id="form-cadastro-categoria" class="col-md-6">
+                            <label for="cxCategoria" class="form-label">Categoria:</label>
+                            <select required type="text" class="form-control campo" name="categoria" id="cxCategoria">
+                                <option disabled selected>Selecione...</option>
+                                <option value="1">Moda</option>
+                                <option value="2">Tecnologia</option>
+                                <option value="3">Eletrodomésticos</option>
+                                <option value="4">Construção</option>
+                                <option value="5">Outros</option>
+                            </select>
+                        </div>
+
+                        <div id="form-cadastro-marca" class="col-md-6">
+                            <label for="cxMarca" class="form-label">Marca:</label>
+                            <input required type="text" class="form-control campo" name="nomeMarca" id="cxMarca">
+                        </div>
+
+
+                        <div id="form-cadastro-produto" class="col-md-6">
+                            <label for="cxProduto" class="form-label">Produto:</label>
+                            <input required type="text" class="form-control campo" name="nomeProduto" minlength="5" maxlength="15" id="cxProduto">
+                        </div>
+
+                        <div id="form-cadastro-preco" class="col-md-6">
+                            <label for="cxPreco" class="form-label">Preço do Lote: </label>
+                            <div class=" input-group" style="max-width: 400px;">
+                                <span class="input-group-text"> R$</span>
+                                <input required type="number" step="0.01" name="preco" min="0.01" class="form-control campo" id="cxPreco">
+                            </div>
+                        </div>
+
+                        <div id="form-cadastro-QIL" class="col-md-6">
+                            <label for="cxQIL" class="form-label">Quantidade de itens do lote:</label>
+                            <input required type="number" class="form-control campo" name="QIL" id="cxQIL">
+                        </div>
+
+                        <div id="form-cadastro-tamanho" class="col-md-6">
+                            <label for="cxTamanho" class="form-label">Tamanho dos itens:</label>
+                            <input required type="text" class="form-control campo" name="tamanho" id="cxTamanho">
+                        </div>
+
+                        <div id="form-cadastro-descricao" class="col-md-6">
+                            <label for="cxDescricao" class="form-label">Descrição:</label>
+                            <input placeholder="Digite aqui..." type="text" class="form-control campo" style="height: 150px;" name="descricao" minlength="5" maxlength="455" id="cxDescricao">
+                        </div>
+
+
+                        <div id="form-cadastro-disponivel" class="col-md-6">
+                            <label for="cxDisponivel" class="form-label">Lotes disponiveis:</label>
+                            <input required type="number" class="form-control campo" name="disponivel" id="cxDisponivel">
+                        </div>
+                        <div id="form-cadastro-imagem" class="col-md-6">
+                            <label for="lblImagem" class="form-label">Adicionar imagem do produto:</label>
+                            <input id="imagem" type="file" name="img" title="Usar arquivo com dimensões 300x300">
+                        </div>
+                        <div id="img-container">
+                            <img id="preview" src="" style="justify-content:center;">
+                        </div>
+
+                        <a> <button id="#btnCadastrarProd" type="button" class="btn btn-outline-primary btn-lg btnCadastrarProd" style="float: right;" method="post">Cadastrar Produto</button> </a>
+                    </div>
+                </div>
+
+
+                <!-- CADASTRAR PRODUTO -->
+
+
+                <?php
+            }
+
+            ?>
+                    <div id="form-alterar">
+            <?php	
+            if (!$linhasProd == null) {
+                foreach ($linhasProd as $linhaProd) {
+
+                ?>
+                        <table class="records">
+                            <thead>
+                                <tr>
+                                    <th>Nome do Produto</th>
+                                    <th> Valor produto</th>
+                                    <th>Ação</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td><?= $linhaProd['Nome do Produto'] ?></td>
+                                    <td><?= $linhaProd['Preco'] ?></td>
+                                    <td id="crud">
+                                        <button id="btn-div" type="submit" class="btn btn-div-prod">visualizar</button>
+                                        <a href="../controller/deletarDadosProd.php?idProduto=<?php echo $linhaProd['idProduto']; ?>"> <button id="btnAlterar" type="submit" class="btn"> Excluir</button></a>
+
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+
+
+                    </div>
+
+                    <div id="container" class="container" style="display: none;">
+                        <form action="../controller/alterarDadosProd.php" method="post" class="row g-3" id="dados">
+                            <div id="form-cadastro-marca" class="col-md-6">
+                                <label for="cxMarca" class="form-label">Marca:</label>
+                                <input type="text" readonly value="<?= $linhaProd['nomeMarca'] ?>" class="form-control" name="nomeMarca" id="cxMarca">
+                            </div>
+
+                        
+                                <input type="hidden" value='<?=$linhaProd['idProduto']?>' name="idProduto">
+                            
+
+
+                            <div id="form-cadastro-produto" class="col-md-6">
+                                <label for="cxProduto" class="form-label">Produto:</label>
+                                <input type="text" class="form-control" name="nomeProduto" maxlength="30" id="cxProduto" value="<?= $linhaProd['Nome do Produto'] ?>">
+                            </div>
+
+                            <div id="form-cadastro-preco" class="col-md-6">
+                                <label for="cxPreco" class="form-label">Preço do Lote: </label>
+                                <div class=" input-group" style="max-width: 400px;">
+                                    <span class="input-group-text"> R$</span>
+                                    <input type="number" step="0.01" name="preco" min="0.01" maxlength="10" class="form-control" id="cxPreco" value="<?= $linhaProd['Preco'] ?>">
+                                </div>
+                            </div>
+
+
+                            <div id="form-cadastro-QIL" class="col-md-6">
+                                <label for="cxQIL" class="form-label">Quantidade de itens do lote:</label>
+                                <input type="number" class="form-control" name="QIL" id="cxQIL" maxlength="6" value="<?= $linhaProd['Quantidade'] ?>">
+                            </div>
+
+                            <div id="form-cadastro-tamanho" class="col-md-6">
+                                <label for="cxTamanho" class="form-label">Tamanho dos itens:</label>
+                                <input type="text" class="form-control" name="tamanho" maxlength="20" id="cxTamanho" value="<?= $linhaProd['Tamanho'] ?>">
+                            </div>
+
+                            <div id="form-cadastro-descricao" class="col-md-6">
+                                <label for="cxDescricao" class="form-label">Descrição:</label>
+                                <input placeholder="Digite aqui..." type="text" class="form-control" maxlength="500" style="height: 150px;" name="descricao" id="cxDescricao" value="<?= $linhaProd['Descricao'] ?>">
+                            </div>
+
+
+                            <div id="form-cadastro-disponivel" class="col-md-6">
+                                <label for="cxDisponivel" class="form-label">Lotes disponiveis:</label>
+                                <input type="number" class="form-control" name="disponivel" maxlength="6" id="cxDisponivel" value="<?= $linhaProd['Disponivel'] ?>">
+                            </div>
+                            <div id="form-cadastro-imagem" class="col-md-6">
+                                <label for="lblImagem" class="form-label">Adicionar imagem do produto:</label>
+                                <input id="img-input" type="file" name="imagem" title="Usar arquivo com dimensões 300x300">
+                            </div>
+                            <div id="img-container">
+                                <img id="preview" src="" style="justify-content:center;">
+                            </div>
+                            <div class="col-12">
+                                <button id="btnAlterar" type="submit" class="btn"> Alterar dados</button>
+                            </div>
+                        </form>
+                    </div>
+                    <?php
+                }
+            }
+?>
+
+    </main>
+
+<!-- JS DIV CADASTRAR -->
+
+<script>
+    var btn = document.getElementById('btn-cad');
+    var container = document.querySelector('.cont');
+    btn.addEventListener('click', function() {
+
+        if (cont.style.display === 'block') {
+            cont.style.display = 'none';
+        } else {
+            cont.style.display = 'block';
+        }
+    });
+</script>
+<!-- JS DIV CADASTRAR -->
+
+<!-- JS DIV VISUALIZAR -->
+
+<script>
+    var btn = document.getElementsByClassName('btn-div-prod')[0];
+    var container = document.querySelector('.container');
+    btn.addEventListener('click', function() {
+
+        if (container.style.display === 'block') {
+            container.style.display = 'none';
+        } else {
+            container.style.display = 'block';
+        }
+    });
+</script>
+
+<!-- JS DIV VISUALIZAR -->
+
+<!-- JS IMAGEM -->
+<script>
+    function readImage() {
+        if (this.files && this.files[0]) {
+            var file = new FileReader();
+            file.onload = function(e) {
+                document.getElementById("preview").src = e.target.result;
+            };
+            file.readAsDataURL(this.files[0]);
+        }
+    }
+
+    document.getElementById("imagem").addEventListener("change", readImage, false);
+</script>
+<!-- JS IMAGEM -->
+
+<!-- JS ESCONDER DIV ALTERAR-->
+<script>
+    
+/* Ajax form Cadastrar produto */
+    let formData = new FormData();
+
+    document.getElementById("imagem").onchange = function(e) {
+        if(e.target.files != null && e.target.length != 0) {
+            formData.append("campoImg", e.target.files[0]);
+        }
+    };
+
+    $('.btnCadastrarProd').click(() => {
+
+        $.each($('.campo'), (index, element) => {
+            formData.append(element.name, element.value)
+        });
+
+        var xml = new XMLHttpRequest();
+        xml.onreadystatechange = function() {
+            if(xml.readyState === 4 && xml.status === 200)
+                alert(xml.responseText);
+        };
+        xml.open("POST", "../controller/cadProduto.php", true);
+        xml.send(formData);
+})
+
+</script>
+<!-- JS ESCONDER ALTERAR-->
+
+</body>
+
+</html>
---
+++ view/cadastrarCli.php
@@ -0,0 +1,156 @@
+<?php
+session_start();
+include_once '../controller/verificaLogado.php';
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../view/css/cadCli.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <title>cadastro</title>
+
+    <script>
+        $(document).ready(() => {
+            $('#cep').keyup(() => {
+            var numCep = $('#cep').val();
+            var url = "https://viacep.com.br/ws/"+numCep+"/json";
+
+            $.ajax({
+                url: url,
+                type: "get",
+                dataType: "json",
+                success: (dados) => {
+                    $('#localidade').val(dados.localidade)
+                    $('#logradouro').val(dados.logradouro);
+                    $('#bairro').val(dados.bairro);
+                    $('#uf').val(dados.uf);
+                }
+
+            })
+        })
+        })
+    </script>
+</head>
+
+<body style="background: -webkit-linear-gradient(to left, rgb(153, 206, 255), rgb( 233, 243, 250));
+
+background: linear-gradient(to left,rgb(153, 206, 255), rgb( 233, 243, 250));">
+    <main>
+        <div class="container" id="container">
+            <div class="form-container">
+                <div id="form-cadastro">
+                    <div class="cadUsu">
+                        <h1>Cadastrar Usuário</h1>
+                        <form action="../controller/cadastrarCli.php" method="post">
+                            <div id="form-cadastro-nome">
+                                <label for="cxNome">Nome:</label>
+                                <input required type="text" name="nome" id="cxNome">
+                            </div>
+
+                            <div id="form-cadastro-sobrenome">
+                                <label for="cxSobrenome">Sobrenome:</label>
+                                <input required type="text" name="sobrenome" id="cxSobrenome">
+                            </div>
+
+                            <div id="form-cadastro-data_nasc">
+                                <label for="cxData_nasc">Data de Nascimento:</label>
+                                <input required type="date" name="data_nasc" id="cxData_nasc">
+                            </div>
+
+                            <div id="form-cadastro-ddd1">
+                                <label for="cxDdd1">DDD</label>
+                                <input maxlength="2" required type="text" name="ddd1" id="cxDdd1">
+                            </div>
+
+                            <div id="form-cadastro-tel1">
+                                <label for="cxTel1">Celular</label>
+                                <input maxlength="10" required type="text" name="tel1" id="cxTel1">
+                            </div>
+
+                            <div id="form-cadastro-ddd2">
+                                <Strong>Opcional</Strong>
+                                <label for="cxDdd1">DDD</label>
+                                <input maxlength="2" type="text" name="ddd2" id="cxDdd2">
+                            </div>
+
+                            <div id="form-cadastro-tel2">
+                                <Strong>Opcional</Strong>
+                                <label for="cxTel2">Celular</label>
+                                <input maxlength="10" type="text" name="tel2" id="cxTel2">
+                            </div>
+
+                            <div id="form-cadastro-cnpj">
+                                <label for="cxCNPJ">CNPJ:</label>
+                                <input maxlength="18" required type="text" name="cnpj" id="cxCNPJ">
+                            </div>
+
+                            <div id="form-cadastro-ramo">
+                                <label for="cxRamo">Ramo da empresa:</label>
+                                <input maxlength="18" required type="text" name="ramo" id="cxRamo">
+                            </div>
+
+                            <div id="form-cadastro-email">
+                                <label for="cxEmail">Email</label>
+                                <input required type="email" name="email" id="cxEmail">
+                            </div>
+
+                            <div id="form-cadastro-senha">
+                                <label for="cxSenha">Senha:</label>
+                                <input required type="password" name="senha" id="cxSenha">
+                            </div>
+                    </div>
+                    <div class="cadEnder">
+                        <h1>Cadastrar endereço</h1>
+                        <div id="form-cadastro-cep">
+                            <label for="cxCep">CEP:</label>
+                            <input maxlength="8" required type="text" name="cep" id="cep">
+                        </div>
+
+                        <div id="form-cadastro-rua">
+                            <label for="cxRua">Rua:</label>
+                            <input readonly type="text" id="logradouro">
+                        </div>
+
+                        <div id="form-cadastro-numero">
+                            <label for="cxNumero">N°:</label>
+                            <input required maxlength="5" type="number" name="numero" id="cxNumero">
+                        </div>
+
+                        <div id="form-cadastro-complemento">
+                            <label for="cxComplemento">complemento:</label>
+                            <input type="text" name="complemento" id="cxComplemento">
+                        </div>
+
+                        <div id="form-cadastro-cidade">
+                            <label for="cxCidade">Cidade:</label>
+                            <input readonly type="text" id="localidade">
+                        </div>
+
+                        <div id="form-cadastro-bairro">
+                            <label for="cxBairro">Bairro:</label>
+                            <input readonly type="text" id="bairro">
+                        </div>
+
+                        <div id="form-cadastro-UF">
+                            <label for="cxUF">UF:</label>
+                            <input readonly type="text" id="uf">
+                        </div>
+                    </div>
+                    <button type="submit">Cadastrar</button>
+                    <button><a href="../index.php" style="color: white;">Voltar</a></button>
+                    </form>
+                </div>
+
+
+
+            </div>
+        </div>
+
+    </main>
+</body>
+
+</html>
